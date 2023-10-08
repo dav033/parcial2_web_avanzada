77c7e6a62368e03b3f8ad3ee02b8cd880a95dc7f
dav033 <87620700+dav033@users.noreply.github.com>
2023-10-07 19:15:46 -0500

    add role model

---
+++ src/models/Role.py
@@ -0,0 +1,21 @@
+from Config.db import db, ma, app
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    CompensationPerProduct = db.Column(db.Float)
+    employee = db.relationship('Employee', backref='roles', lazy=True)
+    
+    def __init__(self, name , role):
+        self.name = name
+        self.role = role
+        
+        
+with app.app_context():
+    db.create_all()
+    
+class RoleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'CompensationPerProduct')
