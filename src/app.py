e71e80e0cdae3576c3dccc0243b63d8ec303a723
dav033 <87620700+dav033@users.noreply.github.com>
2023-10-08 21:59:21 -0500

    remove comments

--- src/app.py
+++ src/app.py
@@ -5,8 +5,6 @@ from controllers.RoleController import role_routes
 from controllers.ProductController import product_routes
 from controllers.AuthController import auth_routes
 from Config.db import db, ma, app
-# app.register_blueprint(ruta_cliente, url_prefix = "/api" )
-# app.register_blueprint(ruta_prestamo, url_prefix = "/api" )
 
 app.register_blueprint(auth_routes, url_prefix = "/api" )
 app.register_blueprint(user_routes, url_prefix = "/api" )
