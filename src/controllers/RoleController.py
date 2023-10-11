81f28b73dd7a0b3ffd9721643a6c44be3d607da5
dav033 <87620700+dav033@users.noreply.github.com>
2023-10-11 13:05:51 -0500

    Remove verify_token function of the routes

--- src/controllers/RoleController.py
+++ src/controllers/RoleController.py
@@ -8,31 +8,23 @@ role_routes = Blueprint("role_routes", __name__)
 
 @role_routes.route("/get_roles", methods=["GET"])
 def get_roles():
-    token_succes = Security.verify_token(request.headers)
-    if (token_succes):
-        roles = role_service.get_roles()
-        return jsonify(roles), 200
-    else:
-        return "Invalid Token", 401
+
+    roles = role_service.get_roles()
+    return jsonify(roles), 200
 
 
 @role_routes.route("/create_role", methods=["POST"])
 def create_role():
-    token_succes = Security.verify_token(request.headers)
-    if (token_succes):
-        data = request.get_json()
-        name = data.get("name")
-        compensation_per_product = data.get("compensationPerProduct")
-        new_role = role_service.create_role(
-            name, compensation_per_product)
-        return "The role has been created", 201
+    data = request.get_json()
+    name = data.get("name")
+    compensation_per_product = data.get("compensationPerProduct")
+    new_role = role_service.create_role(
+        name, compensation_per_product)
 
 
 @role_routes.route("/modify_role", methods=["PUT"])
 def modify_role():
-    token_succes = Security.verify_token(request.headers)
-    if (token_succes):
-        data = request.get_json()
+    data = request.get_json()
     role_id = data.get("rolId")
     name = data.get("name")
     compensation_per_product = data.get("compensationPerProduct")
@@ -40,5 +32,3 @@ def modify_role():
                                              name, compensation_per_product)
     if modified_role != None:
         return "The role has been modified", 201
-    else:
-        return "Not authorized ", 401
--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -8,20 +8,19 @@ user_routes = Blueprint("user_routes", __name__)
 
 @user_routes.route("/create_user", methods=["POST"])
 def create_user():
-    token_succes = Security.verify_token(request.headers)
 
-    if(token_succes):
-      data = request.get_json()
-      name = data.get("name")
-      role = data.get("role")
-      email = data.get("email")
-      password = data.get("password")
-      new_user = user_service.create_user(
+    data = request.get_json()
+    name = data.get("name")
+    role = data.get("role")
+    email = data.get("email")
+    password = data.get("password")
+    new_user = user_service.create_user(
         name, role, email, password)
     return "The user has been created", 201
+
 
 @user_routes.route("/search_user", methods=["GET"])
 def search_user():
     user_id = request.args.get('user_id')
     user = user_service.search_user(user_id)
-    return user.to_json(), 200 
+    return user.to_json(), 200
